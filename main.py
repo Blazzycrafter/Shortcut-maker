cb6efc2d38db25aad766e17a2fe7e7da2820ec88
Blazzycrafter <39300111+Blazzycrafter@users.noreply.github.com>
2024-04-02 09:30:31 +0200

    Now Init

---
+++ main.py
@@ -0,0 +1,43 @@
+# imports
+import os
+
+
+# classes
+class BCOLOR:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+# global variables
+DEBUG = True
+
+
+def dprint(*args, **kwargs):
+    if DEBUG: print(*args, **kwargs)
+
+
+
+
+
+
+def init():
+    # get user name
+    username = os.getlogin()
+    dprint(f'Username: {username}')
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    init()
